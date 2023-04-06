762591897875787c670559fc2d722b773b3af568
Jesus Zuluaga Moreno <citozuluaga44@gmail.com>
2023-04-06 00:19:12 +0000

    Añadida función de reiniciar el grafo

--- processing.py
+++ processing.py
@@ -1,5 +1,5 @@
 import unicodedata 
-
+import copy
 # Taken from stackoverflow and Sydney(Bing)
 def remove_accents(s):
     """
@@ -30,33 +30,93 @@ guide = {"santiago":"chile","london":"united kingdom","la paz":"bolivia",
 
 
 
-def llenar_grafo():
-    f = open("data/airports.csv","r",encoding="utf-8") # open airports database
-    from Grafo import Grafo 
+# def llenar_grafo():
+#     f = open("data/airports.csv","r",encoding="utf-8") # open airports database
+#     from Grafo import Grafo 
 
-    grafo = Grafo()
-    for line in f:
+#     grafo = Grafo()
+#     for line in f:
 
-        data = line.split(",")
-        country = data[3].strip('"').lower()
-        city = data[2].lower()
+#         data = line.split(",")
+#         country = data[3].strip('"').lower()
+#         city = data[2].lower()
 
-        if city in capitals and not check[city] and line.split(",")[4] !="\\N":
-            # take cities that are capitals we have not checked and that have IATA code
-            str = f"{data[1]},{data[2]},{data[3]},{data[4]},{data[6]},{data[7]}"
-            info = [x.strip('"') for x in str.split(",")]
+#         if city in capitals and not check[city] and line.split(",")[4] !="\\N":
+#             # take cities that are capitals we have not checked and that have IATA code
+#             str = f"{data[1]},{data[2]},{data[3]},{data[4]},{data[6]},{data[7]}"
+#             info = [x.strip('"') for x in str.split(",")]
 
-            if city.strip('"') in list(guide.keys()): # if city is special case
-                #name,city,country,code,lat,long
+#             if city.strip('"') in list(guide.keys()): # if city is special case
+#                 #name,city,country,code,lat,long
 
-                if country==guide[city.strip('"')]: # add if we have right country
-                    grafo.addAirport(info)
-                    check[city] = True
-            else:
+#                 if country==guide[city.strip('"')]: # add if we have right country
+#                     grafo.addAirport(info)
+#                     check[city] = True
+#             else:
+#                 grafo.addAirport(info)
+#                 check[city] = True
+#                 #check city as added
+
+#     f.close() 
+#     return grafo 
+# grafo = llenar_grafo()
+
+f = open("data/airports.csv","r",encoding="utf-8") # open airports database
+from Grafo import Grafo 
+
+grafo = Grafo()
+
+for line in f:
+
+    data = line.split(",")
+    country = data[3].strip('"').lower()
+    city = data[2].lower()
+
+    if city in capitals and not check[city] and line.split(",")[4] !="\\N":
+        # take cities that are capitals we have not checked and that have IATA code
+        str = f"{data[1]},{data[2]},{data[3]},{data[4]},{data[6]},{data[7]}"
+        info = [x.strip('"') for x in str.split(",")]
+
+        if city.strip('"') in list(guide.keys()): # if city is special case
+            #name,city,country,code,lat,long
+
+            if country==guide[city.strip('"')]: # add if we have right country
                 grafo.addAirport(info)
                 check[city] = True
-                #check city as added
+        else:
+            grafo.addAirport(info)
+            check[city] = True
+            #check city as added
 
-    f.close() 
-    return grafo 
-grafo = llenar_grafo()
+# Grafo temporal donde copiamos toda la información del grafo original
+grafo_copy = copy.deepcopy(grafo)
+
+# Reinicia el grafo
+def restartGraph(grafo: Grafo, grafo_rep: Grafo):
+    '''
+        Reinicia el grafo
+
+        Args:
+            grafo     (Graph): [Donde queremos devolver toda la información]
+            grafo_rep (Graph): [Donde se encuentra toda la información]
+    '''
+    grafo = grafo_rep
+    return grafo
+
+f.close() 
+
+# Tamaño antes de borrar
+print(len(list(grafo.vertices)))
+
+grafo.deleteAirport('colombia')
+grafo.deleteAirport('mexico')
+grafo.deleteAirport('venezuela')
+grafo.deleteAirport('france')
+
+# Tamaño despues de borrar
+print(len(list(grafo.vertices)))
+
+grafo = restartGraph(grafo, grafo_copy)
+
+# Tamaño al reiniciar el grafo
+print(len(list(grafo.vertices)))
